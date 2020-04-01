6354d7f2a0c2be0719ed40fb43109191374589ab
RyanChen <ryanchen2525@gmail.com>
2020-03-31 23:02:11 -0400

    data transformation

--- android-kotlin-fundamentals-starter-apps-master/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/GameViewModel.kt
+++ android-kotlin-fundamentals-starter-apps-master/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/GameViewModel.kt
@@ -1,15 +1,31 @@
 package com.example.android.guesstheword
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    companion object {
+        private const val DONE = 0L
+
+        private const val ONE_SECOND = 1000L
+
+        // total game
+        private const val COUNTDOWN_TIME = 60000L
+    }
 
     // The current word
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
+
+    val wordHint = Transformations.map(word) {
+        "Current word has " + it.length + " letters"
+    }
 
     // The current score
     private val _score = MutableLiveData<Int>()
@@ -20,12 +36,74 @@ class GameViewModel : ViewModel() {
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+    //count down time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) {
+        DateUtils.formatElapsedTime(it)
+    }
+
+    private val timer: CountDownTimer
+
     init {
         _word.value = ""
         _score.value = 0
 
         resetList()
         nextWord()
+
+        // create timer
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+
+            override fun onTick(p0: Long) {
+                _currentTime.value = p0 / ONE_SECOND
+            }
+
+        }
+        timer.start()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (wordList.isNotEmpty()) {
+            //Select and remove a word from the list
+            _word.value = wordList.removeAt(0)
+        } else {
+            resetList()
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        _score.value = _score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = _score.value?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
     }
 
     // The list of words - the front of the list is the next word to guess
@@ -49,38 +127,6 @@ class GameViewModel : ViewModel() {
                 "guitar"
         )
         wordList.shuffle()
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        if (wordList.isNotEmpty()) {
-            //Select and remove a word from the list
-            _word.value = wordList.removeAt(0)
-        } else {
-            onGameFinish()
-        }
-    }
-
-    /** Methods for buttons presses **/
-
-    fun onSkip() {
-        _score.value = _score.value?.minus(1)
-        nextWord()
-    }
-
-    fun onCorrect() {
-        _score.value = _score.value?.plus(1)
-        nextWord()
-    }
-
-    fun onGameFinish() {
-        _eventGameFinish.value = true
-    }
-
-    fun onGameFinishComplete() {
-        _eventGameFinish.value = false
     }
 
 
